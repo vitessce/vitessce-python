e4a70e44da88b77a4386c26de97ce6c51b352918
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-02-05 23:37:48 -0500

    Fix ordering bugs

--- demos/human_lymph_node_10x_visium/src/create_zarr.py
+++ demos/human_lymph_node_10x_visium/src/create_zarr.py
@@ -21,60 +21,41 @@ def create_zarr(output_path):
     adata = adata[adata.obs["pct_counts_mt"] < 20]
     sc.pp.filter_genes(adata, min_cells=10)
 
-
-
     # Perform normalization
     sc.pp.normalize_total(adata, inplace=True)
     sc.pp.log1p(adata)
     # Determine the top 2000 highly variable genes.
     sc.pp.highly_variable_genes(adata, flavor="seurat", n_top_genes=300)
 
-
-    # Hierarchical clustering of genes for optimal gene ordering
-    X_hvg_arr = adata[:, adata.var['highly_variable']].X.copy().toarray()
-    X_hvg_index = adata[:, adata.var['highly_variable']].var.copy().index
-
-    Z = scipy.cluster.hierarchy.linkage(X_hvg_arr.T, method="average", optimal_ordering=True)
-    labels = X_hvg_index.values
-
-    # Get the hierarchy-based ordering of genes.
-    leaf_index_list = scipy.cluster.hierarchy.leaves_list(Z)
-    leaf_list = labels[leaf_index_list].tolist()
-
-    # Create a new *ordered* gene expression dataframe.
-    X_hvg_df = pd.DataFrame(
-        index=adata.obs.index.values.tolist(),
-        columns=X_hvg_index.values.tolist(),
-        data=X_hvg_arr
-    )
-    X_hvg_ordered_df = X_hvg_df[leaf_list]
-
-    highly_variable_ordered = leaf_list
-    not_variable = set(adata.var.index.values.tolist()) - set(highly_variable_ordered)
-    highly_variable_ordered_plus_not_variable = highly_variable_ordered + list(not_variable)
-
-    X_df = pd.DataFrame(
-        index=adata.obs.index.values.tolist(),
-        columns=adata.var.index.values.tolist(),
-        data=adata.X.toarray()
-    )
-    X_ordered_df = X_df[highly_variable_ordered_plus_not_variable]
-
-    var_df = adata.var
-    var_ordered_df = var_df.loc[highly_variable_ordered_plus_not_variable]
-
-    adata_uns_spatial = adata.uns['spatial']
-
-    # Create a new AnnData object with *ordered* genes.
-    adata = AnnData(X=X_ordered_df.values, var=var_ordered_df.copy(), obs=adata.obs.copy())
-    adata.obsm['X_hvg'] = X_hvg_ordered_df.values
-    adata.uns['spatial'] = adata_uns_spatial
-
     # Dimensionality reduction and clustering
     sc.pp.pca(adata)
     sc.pp.neighbors(adata)
     sc.tl.umap(adata)
     sc.tl.leiden(adata, key_added="clusters")
+
+
+    # Hierarchical clustering of genes for optimal gene ordering
+    X_hvg_arr = adata[:, adata.var['highly_variable']].X.toarray()
+    X_hvg_index = adata[:, adata.var['highly_variable']].var.copy().index
+
+    Z = scipy.cluster.hierarchy.linkage(X_hvg_arr.T, method="average", optimal_ordering=True)
+
+    # Get the hierarchy-based ordering of genes.
+    num_genes = adata.var.shape[0]
+    highly_var_index_ordering = scipy.cluster.hierarchy.leaves_list(Z)
+    highly_var_genes = X_hvg_index.values[highly_var_index_ordering].tolist()
+
+    all_genes = adata.var.index.values.tolist()
+    not_var_genes = adata.var.loc[~adata.var['highly_variable']].index.values.tolist()
+
+    def get_orig_index(gene_id):
+        return all_genes.index(gene_id)
+    
+    var_index_ordering = list(map(get_orig_index, highly_var_genes)) + list(map(get_orig_index, not_var_genes))
+
+    # Create a new *ordered* gene expression dataframe.
+    adata = adata[:, var_index_ordering].copy()
+    adata.obsm["X_hvg"] = adata[:, adata.var['highly_variable']].X.copy()
 
     adata.obsm['xy'] = np.stack((adata.obs['array_col'].values, adata.obs['array_row'].values), axis=-1).astype('uint8')
 

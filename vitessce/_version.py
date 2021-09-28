4ee57f7db21a72bf4bfb80875bfed32a0517f8e0
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-09-28 16:49:31 -0400

    Try again

--- vitessce/_version.py
+++ vitessce/_version.py
@@ -1,5 +1,5 @@
 # Module version
-version_info = (1, 0, 2)
+version_info = (1, 0, 3)
 
 # Module version accessible using vitessce.__version__
 __version__ = '%s.%s.%s'%(version_info[0], version_info[1], version_info[2])

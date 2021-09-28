a011a4d4fce0d3cf49f3b2bfcf5f429ef17e951e
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-09-28 11:43:39 -0400

    Use latest cookiecutter

--- vitessce/__init__.py
+++ vitessce/__init__.py
@@ -20,6 +20,25 @@ from .export import (
     export_to_files,
 )
 
+def _jupyter_labextension_paths():
+    """Called by Jupyter Lab Server to detect if it is a valid labextension and
+    to install the widget
+
+    Returns
+    =======
+    src: Source directory name to copy files from. Webpack outputs generated files
+        into this directory and Jupyter Lab copies from this directory during
+        widget installation
+    dest: Destination directory name to install widget files to. Jupyter Lab copies
+        from `src` directory into <jupyter path>/labextensions/<dest> directory
+        during widget installation
+    """
+    return [{
+        'src': 'labextension',
+        'dest': 'vitessce-jupyter',
+    }]
+
+
 def _jupyter_nbextension_paths():
     """Called by Jupyter Notebook Server to detect if it is a valid nbextension and
     to install the widget
@@ -39,7 +58,7 @@ def _jupyter_nbextension_paths():
     """
     return [{
         'section': 'notebook',
-        'src': 'static',
+        'src': 'nbextension',
         'dest': 'vitessce-jupyter',
         'require': 'vitessce-jupyter/extension'
     }]

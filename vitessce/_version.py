3b064317791fd942fe0a4895705a0ad71d466322
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-11-18 12:15:45 -0500

    Bump js version for jupyter widget package

--- vitessce/_version.py
+++ vitessce/_version.py
@@ -1,6 +1,6 @@
 # Module version
 py_version_info = (1, 0, 4)
-js_version_info = (0, 1, 8)
+js_version_info = (0, 1, 9)
 
 # Module version accessible using vitessce.__version__
 __version__ = '%s.%s.%s' % (py_version_info[0], py_version_info[1], py_version_info[2])

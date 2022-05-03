3ec1266b8c6e96adb4281b13f10a43cc4d72e26c
mccalluc <mccalluc@users.noreply.github.com>
2022-05-03 11:03:04 -0400

    new notebook extra dependencies

--- setup.py
+++ setup.py
@@ -77,6 +77,19 @@ extras_require = {
     ],
     'proxy': [
         'jupyter-server-proxy>=1.5.2'
+    ],
+    'notebook': [
+        # Needed only for notebook use:
+        'ipywidgets>=7.6.0',
+        'hypercorn>=0.11.0',
+        'ujson>=4.0.1',
+        'starlette==0.14.0',
+
+        # aiofiles is not explicitly referenced in our code,
+        # but it is an implicit dependency of starlette==0.14.0.
+        # https://github.com/encode/starlette/issues/49
+        # Upgrading starlette will remove this dependency.
+        'aiofiles>=0.6.0'
     ]
 }
 
@@ -94,10 +107,6 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     include_package_data=True,
     install_requires=[
-        'ipywidgets>=7.6.0',
-        'hypercorn>=0.11.0',
-        'ujson>=4.0.1',
-        'starlette==0.14.0',
         'zarr>=2.5.0',
         'numcodecs>=0.5.7',
         'scipy>=1.2.1',
@@ -106,12 +115,6 @@ setup_args = dict(
         'numpy>=1.21.2',
         'pandas>=1.1.2',
         'black>=21.11b1',
-
-        # aiofiles is not explicitly referenced in our code,
-        # but it is an implicit dependency of starlette==0.14.0.
-        # https://github.com/encode/starlette/issues/49
-        # Upgrading starlette will remove this dependency.
-        'aiofiles>=0.6.0'
     ],
     extras_require=extras_require,
     packages=find_packages(),

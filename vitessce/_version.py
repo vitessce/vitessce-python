d6116361a623b7deef459af82a861102908db71f
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-05-09 09:16:31 -0400

    Version bump

--- vitessce/_version.py
+++ vitessce/_version.py
@@ -1,6 +1,6 @@
 # Module version
-py_version_info = (1, 0, 5)
-js_version_info = (0, 1, 10)
+py_version_info = (1, 0, 6)
+js_version_info = (0, 1, 11)
 
 # Module version accessible using vitessce.__version__
 __version__ = '%s.%s.%s' % (

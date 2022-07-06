0a710d6b5353a4bd17a41909ef8390ee2bff4201
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-07-06 09:45:26 -0400

    Update

--- vitessce/_version.py
+++ vitessce/_version.py
@@ -1,6 +1,9 @@
-# Module version
-py_version_info = (1, 0, 8)
-js_version_info = (0, 1, 13)
+# The version of the Python package (`vitessce` on PyPI).
+py_version_info = (1, 0, 9)
+# The version of the JS package, should match the value in js/package.json (`vitessce-jupyter` on NPM).
+js_version_info = (0, 1, 14)
+# The maximum view config schema version supported by the bundled Vitessce JS package js/package.json.
+vc_version_info = (1, 0, 7)
 
 # Module version accessible using vitessce.__version__
 __version__ = '%s.%s.%s' % (

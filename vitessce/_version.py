c2327be29280329de5ca6fb7a634f7d7e8a2402b
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-07-06 09:47:00 -0400

    Make release

--- vitessce/_version.py
+++ vitessce/_version.py
@@ -1,6 +1,6 @@
 # Module version
-py_version_info = (1, 0, 8)
-js_version_info = (0, 1, 13)
+py_version_info = (1, 0, 9)
+js_version_info = (0, 1, 14)
 
 # Module version accessible using vitessce.__version__
 __version__ = '%s.%s.%s' % (

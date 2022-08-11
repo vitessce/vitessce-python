ce4a0b2e31d958a06378c675e65a4ec7fb1e1500
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-08-11 10:57:32 -0400

    Lint

--- demos/combat_2022_cell/src/convert_h5ad_to_zarr.py
+++ demos/combat_2022_cell/src/convert_h5ad_to_zarr.py
@@ -1,17 +1,17 @@
 import argparse
-import json
 from anndata import read_h5ad
 import numpy as np
 
 
 def to_uint8(arr):
-    arr *= 255.0/arr.max()
+    arr *= 255.0 / arr.max()
     arr = arr.astype(np.dtype('uint8')).todense()
     return arr
 
+
 def convert_h5ad_to_zarr(input_path, output_path):
     adata = read_h5ad(input_path)
-    
+
     adata.var['is_gene_expression'] = adata.var['feature_types'] == 'Gene Expression'
     adata.var['is_antibody_capture'] = adata.var['feature_types'] == 'Antibody Capture'
 
@@ -21,6 +21,7 @@ def convert_h5ad_to_zarr(input_path, output_path):
     adata.obsm['X_umap'] = adata.obsm['X_umap'].astype(np.dtype('<f4'))
 
     adata.write_zarr(output_path)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()

27d0121641f7be0e3d22fc9ec81ac2cf64528d3e
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-08-11 10:47:26 -0400

    Lint

--- demos/codeluppi_2018_nature_methods/src/convert_to_csv.py
+++ demos/codeluppi_2018_nature_methods/src/convert_to_csv.py
@@ -1,17 +1,11 @@
 import argparse
 import json
-from anndata import read_h5ad
-import numpy as np
 import pandas as pd
 
 
 def convert_to_csv(args):
     with open(args.input_cells) as f:
         cells_json = json.load(f)
-    with open(args.input_cell_sets) as f:
-        cell_sets_json = json.load(f)
-    with open(args.input_raster) as f:
-        raster_json = json.load(f)
     with open(args.input_molecules) as f:
         molecules_json = json.load(f)
     with open(args.input_expression_matrix) as f:
@@ -20,14 +14,14 @@ def convert_to_csv(args):
     molecules_df = pd.DataFrame(index=[], columns=["X", "Y", "Gene"])
     start_at = 0
     for molecule_type, molecule_arr in molecules_json.items():
-        molecule_type_ids = [ str(i) for i in range(start_at, start_at + len(molecule_arr)) ]
+        molecule_type_ids = [str(i) for i in range(start_at, start_at + len(molecule_arr))]
         start_at += len(molecule_arr)
         molecule_type_df = pd.DataFrame(index=molecule_type_ids, columns=molecules_df.columns.values.tolist())
         molecule_type_df['Gene'] = molecule_type
-        molecule_type_df['X'] = [ m[0] for m in molecule_arr ]
-        molecule_type_df['Y'] = [ m[1] for m in molecule_arr ]
+        molecule_type_df['X'] = [m[0] for m in molecule_arr]
+        molecule_type_df['Y'] = [m[1] for m in molecule_arr]
 
-        molecules_df = pd.concat([ molecules_df, molecule_type_df ])
+        molecules_df = pd.concat([molecules_df, molecule_type_df])
     molecules_df.index = molecules_df.index.rename('molecule_id')
 
     cell_ids = cells_json.keys()
@@ -41,11 +35,11 @@ def convert_to_csv(args):
     cells_df['Cluster'] = cells_df.apply(lambda row: cells_json[str(row.name)]['factors']['cluster'], axis='columns')
     cells_df['Subcluster'] = cells_df.apply(lambda row: cells_json[str(row.name)]['factors']['subcluster'], axis='columns')
     cells_df.index = cells_df.index.rename('cell_id')
-    
+
     segmentations = dict()
     for cell_id, cell_obj in cells_json.items():
         segmentations[cell_id] = cell_obj['poly']
-    
+
     matrix_df = pd.DataFrame(index=expression_matrix['cols'], columns=expression_matrix['rows'])
     for i, gene in enumerate(matrix_df.columns.values.tolist()):
         matrix_df[gene] = expression_matrix['matrix'][i]
@@ -54,10 +48,9 @@ def convert_to_csv(args):
     cells_df.to_csv(args.output_cells, index=True)
     matrix_df.to_csv(args.output_cells_matrix, index=True)
     molecules_df.to_csv(args.output_molecules, index=True)
-    
+
     with open(args.output_cells_segmentations, 'w') as f:
         json.dump(segmentations, f)
-    
 
 
 if __name__ == '__main__':
@@ -68,20 +61,6 @@ if __name__ == '__main__':
         type=str,
         required=True,
         help='Input cells.json file'
-    )
-    parser.add_argument(
-        '-ics',
-        '--input_cell_sets',
-        type=str,
-        required=True,
-        help='Input cell-sets.json file'
-    )
-    parser.add_argument(
-        '-ir',
-        '--input_raster',
-        type=str,
-        required=True,
-        help='Input raster.json file'
     )
     parser.add_argument(
         '-im',

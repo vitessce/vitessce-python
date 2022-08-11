971a688f8030046c98cc051418b9265cbb587d11
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-08-11 11:34:17 -0400

    Add create demo script

---
+++ demos/create_demo.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+(( $# == 1 )) || die "Requires one argument, the demo name; Instead got $#."
+
+mkdir "./$1"
+mkdir "./$1/src"
+touch "./$1/Snakefile"
+touch "./$1/config.yml"
+touch "./$1/vitessce.json"
+
+cat << EOF > "./$1/Snakefile"
+include: "../common.smk"
+configfile: "config.yml"
+
+rule all:
+    input:
+        config['output']
+EOF
+
+cat << EOF > "./$1/config.yml"
+output:
+- $1.h5ad.zarr
+EOF
+
+cat << EOF
+Add the following to the main demos Snakefile:
+
+subworkflow $1:
+    workdir:
+        "$1"
+EOF

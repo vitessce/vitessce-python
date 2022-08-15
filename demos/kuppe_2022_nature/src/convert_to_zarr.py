1dc4f7070de93e5917653899da261388dcfa8061
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-08-15 16:01:01 -0400

    Update data processing

--- demos/kuppe_2022_nature/src/convert_to_zarr.py
+++ demos/kuppe_2022_nature/src/convert_to_zarr.py
@@ -1,15 +1,28 @@
 import argparse
 import scanpy as sc
 import numpy as np
-from anndata import read_h5ad
+import pandas as pd
+from anndata import read_h5ad, AnnData
+
+
+def to_uint8(arr):
+    arr *= 255.0 / arr.max()
+    arr = arr.astype(np.dtype('uint8')).todense()
+    return arr
 
 
 def process_h5ad_files(args):
     rna_adata = read_h5ad(args.input_rna)
     atac_adata = read_h5ad(args.input_atac)
 
-    print(rna_adata)
-    print(atac_adata)
+    rna_adata.X = to_uint8(rna_adata.X)
+    atac_adata.X = to_uint8(atac_adata.X)
+
+    joint_cols = ['cell_type', 'development_stage', 'disease', 'sex']
+    joint_obs_df = pd.concat([ rna_adata.obs[joint_cols], atac_adata.obs[joint_cols] ])
+    
+    joint_adata = AnnData(obs=joint_obs_df)
+    joint_adata.write_zarr(args.output_joint)
 
     rna_adata.write_zarr(args.output_rna)
     atac_adata.write_zarr(args.output_atac)
@@ -45,6 +58,13 @@ if __name__ == '__main__':
         type=str,
         required=True,
         help='Output snATAC-seq zarr store'
+    )
+    parser.add_argument(
+        '-oj',
+        '--output_joint',
+        type=str,
+        required=True,
+        help='Output joint RNA+ATAC zarr store'
     )
     args = parser.parse_args()
     process_h5ad_files(args)

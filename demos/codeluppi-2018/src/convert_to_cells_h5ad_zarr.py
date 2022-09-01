e4c3e97b39fe42a63dc7e2ef012b8194bb349ae4
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-09-01 13:41:46 -0400

    Update codeluppi-to-csv scripts

--- demos/codeluppi-2018/src/convert_to_cells_h5ad_zarr.py
+++ demos/codeluppi-2018/src/convert_to_cells_h5ad_zarr.py
@@ -1,15 +1,11 @@
 import argparse
 from anndata import AnnData, read_loom
 import pickle
-import json
 import numpy as np
 import pandas as pd
 import scanpy as sc
 import geopandas as gpd
-import dask_geopandas as dgpd
-from dask.diagnostics import ProgressBar
-from tqdm import tqdm
-from shapely.geometry import Point, Polygon, shape
+from shapely.geometry import Polygon
 from vitessce.data_utils import (
     to_uint8,
     sort_var_axis,
@@ -18,38 +14,91 @@ from vitessce.data_utils import (
 
 # Taken from http://linnarssonlab.org/osmFISH/clusters/
 LOOKUP = {
-  "Astrocyte Gfap": "Astrocyte",
-  "Astrocyte Mfge8": "Astrocyte",
-  "C. Plexus": "Ventricle",
-  "Endothelial 1": "Vasculature",
-  "Endothelial": "Vasculature",
-  "Ependymal": "Ventricle",
-  "Hippocampus": "Excitatory neurons",
-  "Inhibitory CP": "Inhibitory neurons",
-  "Inhibitory Cnr1": "Inhibitory neurons",
-  "Inhibitory Crhbp": "Inhibitory neurons",
-  "Inhibitory IC": "Inhibitory neurons",
-  "Inhibitory Kcnip2": "Inhibitory neurons",
-  "Inhibitory Pthlh": "Inhibitory neurons",
-  "Inhibitory Vip": "Inhibitory neurons",
-  "Microglia": "Brain immune",
-  "Oligodendrocyte COP": "Oligodendrocytes",
-  "Oligodendrocyte MF": "Oligodendrocytes",
-  "Oligodendrocyte Mature": "Oligodendrocytes",
-  "Oligodendrocyte NF": "Oligodendrocytes",
-  "Oligodendrocyte Precursor cells": "Oligodendrocytes",
-  "Pericytes": "Vasculature",
-  "Perivascular Macrophages": "Brain immune",
-  "Pyramidal Cpne5": "Excitatory neurons",
-  "Pyramidal Kcnip2": "Excitatory neurons",
-  "Pyramidal L2-3 L5": "Excitatory neurons",
-  "Pyramidal L2-3": "Excitatory neurons",
-  "Pyramidal L3-4": "Excitatory neurons",
-  "Pyramidal L5": "Excitatory neurons",
-  "Pyramidal L6": "Excitatory neurons",
-  "Vascular Smooth Muscle": "Vasculature",
-  "pyramidal L4": "Excitatory neurons"
+    "Astrocyte Gfap": "Astrocyte",
+    "Astrocyte Mfge8": "Astrocyte",
+    "C. Plexus": "Ventricle",
+    "Endothelial 1": "Vasculature",
+    "Endothelial": "Vasculature",
+    "Ependymal": "Ventricle",
+    "Hippocampus": "Excitatory neurons",
+    "Inhibitory CP": "Inhibitory neurons",
+    "Inhibitory Cnr1": "Inhibitory neurons",
+    "Inhibitory Crhbp": "Inhibitory neurons",
+    "Inhibitory IC": "Inhibitory neurons",
+    "Inhibitory Kcnip2": "Inhibitory neurons",
+    "Inhibitory Pthlh": "Inhibitory neurons",
+    "Inhibitory Vip": "Inhibitory neurons",
+    "Microglia": "Brain immune",
+    "Oligodendrocyte COP": "Oligodendrocytes",
+    "Oligodendrocyte MF": "Oligodendrocytes",
+    "Oligodendrocyte Mature": "Oligodendrocytes",
+    "Oligodendrocyte NF": "Oligodendrocytes",
+    "Oligodendrocyte Precursor cells": "Oligodendrocytes",
+    "Pericytes": "Vasculature",
+    "Perivascular Macrophages": "Brain immune",
+    "Pyramidal Cpne5": "Excitatory neurons",
+    "Pyramidal Kcnip2": "Excitatory neurons",
+    "Pyramidal L2-3 L5": "Excitatory neurons",
+    "Pyramidal L2-3": "Excitatory neurons",
+    "Pyramidal L3-4": "Excitatory neurons",
+    "Pyramidal L5": "Excitatory neurons",
+    "Pyramidal L6": "Excitatory neurons",
+    "Vascular Smooth Muscle": "Vasculature",
+    "pyramidal L4": "Excitatory neurons",
 }
+
+# Copied from https://github.com/vitessce/vitessce-data/blob/6726dae/python/cell_reader.py#L17
+def octagon(poly):
+    """
+    Returns a bounding octagon.
+    >>> square = np.array([[0,0], [0,1], [1,1], [1,0]])
+    >>> octagon(square)
+    [[0, 0], [0, 1], [0, 1], [1, 1], [1, 1], [1, 0], [1, 0], [0, 0]]
+    >>> triangle = np.array([[1,0], [0,2], [2,3]])
+    >>> octagon(triangle)
+    [[0, 1], [0, 2], [1, 3], [2, 3], [2, 3], [2, 1], [1, 0], [1, 0]]
+    >>> type(octagon(triangle)[0][0])
+    <class 'int'>
+    """
+    # SciPy has ConvexHull, but it segfaulted on me: perhaps
+    #   https://github.com/scipy/scipy/issues/9751
+    # ... and even if I fixed it locally,
+    # not sure we want that fragility.
+    #
+    # Also: The goal is really just to get a simplified shape...
+    # A convex hull is too precise in some ways,
+    # while in others it falls short, ie, concavities.
+    #
+    # I kind-of like the obvious artificiality of an octagon.
+
+    # Was unsigned, and substraction causes underflow.
+    poly_as_int = poly
+    min_x = np.min(poly_as_int[:, [0]])
+    max_x = np.max(poly_as_int[:, [0]])
+    min_y = np.min(poly_as_int[:, [1]])
+    max_y = np.max(poly_as_int[:, [1]])
+
+    summed = np.sum(poly_as_int, axis=1)
+    diffed = np.diff(poly_as_int, axis=1)
+
+    min_sum = np.min(summed)
+    max_sum = np.max(summed)
+    min_diff = np.min(diffed)
+    max_diff = np.max(diffed)
+
+    return Polygon(
+        [
+            [min_x, min_sum - min_x],
+            [min_x, max_diff + min_x],  # ^ Left
+            [max_y - max_diff, max_y],
+            [max_sum - max_y, max_y],  # ^ Botton
+            [max_x, max_sum - max_x],
+            [max_x, min_diff + max_x],  # ^ Right
+            [min_y - min_diff, min_y],
+            [min_sum - min_y, min_y],  # ^ Top
+        ]
+    )
+
 
 def convert_to_cells_h5ad_zarr(args):
     adata = read_loom(
@@ -57,92 +106,84 @@ def convert_to_cells_h5ad_zarr(args):
         obsm_mapping={
             "X_tsne": ["_tSNE_1", "_tSNE_2"],
             "X_spatial": ["X", "Y"],
-        }
+        },
     )
 
-    adata.obs['is_excluded'] = adata.obs['ClusterName'] == 'Excluded'
-    adata = adata[~adata.obs['is_excluded'], :].copy()
+    adata.obs["is_excluded"] = adata.obs["ClusterName"] == "Excluded"
+    adata = adata[~adata.obs["is_excluded"], :].copy()
 
     # Reorder the genes axis after hierarchical clustering.
     leaf_list = sort_var_axis(adata.X, adata.var.index.values)
     adata = adata[:, leaf_list].copy()
 
     # Store expression matrix as uint8
-    adata.layers['X_uint8'] = to_uint8(adata.X, norm_along="var")
+    adata.layers["X_uint8"] = to_uint8(adata.X, norm_along="var")
 
     sc.pp.pca(adata)
     sc.pp.neighbors(adata)
     sc.tl.umap(adata)
 
-    adata.obs['cell_type'] = adata.obs['ClusterName'].apply(lambda val: LOOKUP[val])
-    adata.obsm['X_centroid'] = np.zeros((adata.shape[0], 2))
-
-    segmentation_simple = {}
+    adata.obs = adata.obs.rename(columns={ "ClusterName": "Subcluster" })
+    adata.obs["Cluster"] = adata.obs["Subcluster"].apply(lambda val: LOOKUP[val])
+    
+    adata.obsm["X_centroid"] = np.zeros((adata.shape[0], 2))
+    adata.obsm["X_segmentations"] = np.zeros((adata.shape[0], 8, 2))
 
     with open(args.input_pkl, "rb") as f_in:
         segmentation = pickle.load(f_in)
 
         cell_ids = adata.obs.index.values.tolist()
 
-        print("geoseries")
-        geoseries = gpd.GeoSeries([ Polygon(segmentation[cell_id]) for cell_id in cell_ids ])
-        print("dgs")
-        dgs = dgpd.from_geopandas(geoseries, npartitions=500)
-        print("geoseries_simple")
-        with ProgressBar():
-            geoseries_simple = dgs.simplify(500.0, preserve_topology=True).compute()
-        print("centroids")
+        geoseries = gpd.GeoSeries(
+            index=cell_ids,
+            data=[Polygon(segmentation[cell_id]) for cell_id in cell_ids],
+        )
+        geoseries_simple = geoseries.apply(
+            lambda p: octagon(np.array(p.exterior.coords))
+        )
         centroids = geoseries_simple.centroid
 
-        adata.obsm['X_centroid'][:, 0] = centroids.x
-        adata.obsm['X_centroid'][:, 1] = centroids.y
+        adata.obsm["X_centroid"][:, 0] = centroids.x
+        adata.obsm["X_centroid"][:, 1] = centroids.y
 
         for cell_id in cell_ids:
-            segmentation_simple[cell_id] = geoseries_simple[cell_id].exterior.coords
-           
-    with open(args.output_segmentations, "w") as f_out:
-        json.dump(segmentation_simple, f_out)
-    
+            i = cell_ids.index(cell_id)
+            adata.obsm["X_segmentations"][i, :, :] = np.array(
+                geoseries_simple.loc[cell_id].exterior.coords
+            )[:8, :]
+
     adata = optimize_adata(
         adata,
-        obs_cols=["ClusterName", "cell_type", "Region"],
-        var_cols=["Flourophore", "Hybridization"],
-        obsm_keys=["X_tsne", "X_umap", "X_pca", "X_spatial", "X_centroid"],
+        obs_cols=["Cluster", "Subcluster", "Region"],
+        var_cols=["Fluorophore", "Hybridization"],
+        obsm_keys=[
+            "X_tsne",
+            "X_umap",
+            "X_pca",
+            "X_spatial",
+            "X_centroid",
+            "X_segmentations",
+        ],
         layer_keys=["X_uint8"],
     )
-    
-    adata.write_zarr(args.output_cells)
+
+    adata.write_zarr(args.output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-il',
-        '--input_loom',
-        type=str,
-        required=True,
-        help='Input Loom file'
+        "-il", "--input_loom", type=str, required=True, help="Input Loom file"
     )
     parser.add_argument(
-        '-ip',
-        '--input_pkl',
-        type=str,
-        required=True,
-        help='Input pickle file'
+        "-ip", "--input_pkl", type=str, required=True, help="Input pickle file"
     )
     parser.add_argument(
-        '-oc',
-        '--output_cells',
+        "-o",
+        "--output",
         type=str,
         required=True,
-        help='Output AnnData Zarr store'
-    )
-    parser.add_argument(
-        '-os',
-        '--output_segmentations',
-        type=str,
-        required=True,
-        help='Output JSON file'
+        help="Output AnnData Zarr store",
     )
     args = parser.parse_args()
     convert_to_cells_h5ad_zarr(args)

e5a37559a02654984f9cb1f08569b6ed68a085c8
ilan-gold <ilanbassgold@gmail.com>
2022-12-03 23:02:34 +0100

    (fix): ok, np.array

--- demos/human-lymph-node-10x-visium/src/create_zarr.py
+++ demos/human-lymph-node-10x-visium/src/create_zarr.py
@@ -83,7 +83,7 @@ def create_zarr(output_adata, output_img):
     img_arr *= 255.0
 
     rgb_img_to_ome_zarr(img_arr, output_img, axes="cyx", chunks=(1, 256, 256), img_name="H & E Image")
-    X = to_dense(adata.X)
+    X = adata.X
     adata = optimize_adata(
         adata,
         obs_cols=["clusters"],
@@ -91,7 +91,7 @@ def create_zarr(output_adata, output_img):
         obsm_keys=["spatial", "segmentations", "X_umap", "X_pca"],
         ignore_X=True
     )
-    adata.X = X
+    adata.X = np.array(to_dense(X))
     adata.write_zarr(output_adata, chunks=[adata.shape[0], 10])
 
 

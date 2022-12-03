3c91c47a8300a059aef8c65f26199777fc0330e6
ilan-gold <ilanbassgold@gmail.com>
2022-12-03 22:57:34 +0100

    (fix): use `todense()`

--- demos/human-lymph-node-10x-visium/src/create_zarr.py
+++ demos/human-lymph-node-10x-visium/src/create_zarr.py
@@ -8,6 +8,7 @@ from vitessce.data_utils import (
     rgb_img_to_ome_zarr,
     optimize_adata,
 )
+from vitessce.data_utils.anndata import to_dense
 
 
 def create_zarr(output_adata, output_img):
@@ -82,7 +83,7 @@ def create_zarr(output_adata, output_img):
     img_arr *= 255.0
 
     rgb_img_to_ome_zarr(img_arr, output_img, axes="cyx", chunks=(1, 256, 256), img_name="H & E Image")
-    X = adata.X.todense()
+    X = to_dense(adata.X)
     adata = optimize_adata(
         adata,
         obs_cols=["clusters"],

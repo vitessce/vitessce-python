6458efbb782f63f3718001fd30a3c8d4615c0eff
ilan-gold <ilanbassgold@gmail.com>
2022-12-03 22:52:08 +0100

    (fix): make

--- demos/human-lymph-node-10x-visium/src/create_zarr.py
+++ demos/human-lymph-node-10x-visium/src/create_zarr.py
@@ -82,7 +82,7 @@ def create_zarr(output_adata, output_img):
     img_arr *= 255.0
 
     rgb_img_to_ome_zarr(img_arr, output_img, axes="cyx", chunks=(1, 256, 256), img_name="H & E Image")
-    X = adata.X
+    X = adata.X.tocsc()
     adata = optimize_adata(
         adata,
         obs_cols=["clusters"],

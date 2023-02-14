b723587ae1f5288bba63270d9d633cc1df5a119c
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-02-14 17:28:56 -0500

    Refactor genomic profiles, move into data_utils

--- vitessce/wrappers.py
+++ vitessce/wrappers.py
@@ -14,7 +14,6 @@ from .constants import (
     ViewType as cm,
     FileType as ft,
 )
-from .entities import Cells, CellSets, GenomicProfiles
 from .repr import make_repr
 
 
@@ -579,8 +578,7 @@ class AnnDataWrapper(AbstractWrapper):
                     options["obsFeatureMatrix"]["initialFeatureFilterPath"] = self._matrix_gene_var_filter
                 if self._gene_alias is not None:
                     options["featureLabels"] = {
-                        "path": self._gene_alias,
-                        "featureLabelsType": "feature"
+                        "path": self._gene_alias
                     }
             if len(options.keys()) > 0:
                 obj_file_def = {
@@ -614,278 +612,52 @@ class AnnDataWrapper(AbstractWrapper):
                       / heatmap)
 
 
-class SnapWrapper(AbstractWrapper):
+class MultivecZarrWrapper(AbstractWrapper):
 
-    # The Snap file is difficult to work with.
-    # For now we can use the processed cell-by-bin MTX file
-    # However, the HuBMAP pipeline currently computes this with resolution 5000
-    # TODO: Make a PR to sc-atac-seq-pipeline to output this at a higher resolution (e.g. 200)
-    # https://github.com/hubmapconsortium/sc-atac-seq-pipeline/blob/develop/bin/snapAnalysis.R#L93
-
-    def __init__(self, in_mtx, in_barcodes_df, in_bins_df, in_clusters_df, starting_resolution=5000, **kwargs):
+    def __init__(self, zarr_path=None, zarr_url=None, **kwargs):
         super().__init__(**kwargs)
         self._repr = make_repr(locals())
-        # scipy.sparse.coo.coo_matrix (filtered_cell_by_bin.mtx)
-        self.in_mtx = in_mtx
-        self.in_barcodes_df = in_barcodes_df  # pandas dataframe (barcodes.txt)
-        self.in_bins_df = in_bins_df  # pandas dataframe (bins.txt)
-        # pandas dataframe (umap_coords_clusters.csv)
-        self.in_clusters_df = in_clusters_df
-        self.zarr_folder = 'profiles.zarr'
-
-        self.starting_resolution = starting_resolution
-
-        # Convert to dense matrix if sparse.
-        if isinstance(in_mtx, coo_matrix):
-            self.in_mtx = in_mtx.toarray()
+        if zarr_url is not None and zarr_path is not None:
+            raise ValueError(
+                "Did not expect zarr_path to be provided with zarr_url")
+        if zarr_url is None and zarr_path is None:
+            raise ValueError(
+                "Expected either zarr_url or zarr_path to be provided")
+        self._zarr_path = zarr_path
+        self._zarr_url = zarr_url
+        if self._zarr_path is not None:
+            self.is_remote = False
+        else:
+            self.is_remote = True
+        self.local_dir_uid = str(uuid4())
 
     def convert_and_save(self, dataset_uid, obj_i):
-        super().convert_and_save(dataset_uid, obj_i)
-        out_dir = self._get_out_dir(dataset_uid, obj_i)
-        zarr_filepath = join(out_dir, self.zarr_folder)
+        # Only create out-directory if needed
+        if not self.is_remote:
+            super().convert_and_save(dataset_uid, obj_i)
 
-        self.create_genomic_multivec_zarr(zarr_filepath)
-        with open(join(out_dir, 'cell-sets'), 'w') as f:
-            f.write(json.dumps(self.create_cell_sets_json()))
-        with open(join(out_dir, 'cells'), 'w') as f:
-            f.write(json.dumps(self.create_cells_json()))
-
-        cells_file_creator = self.make_cells_file_def_creator(
+        file_def_creator = self.make_genomic_profiles_file_def_creator(
             dataset_uid, obj_i)
-        cell_sets_file_creator = self.make_cell_sets_file_def_creator(
-            dataset_uid, obj_i)
-        genomic_profiles_file_creator = self.make_genomic_profiles_file_def_creator(
-            dataset_uid, obj_i)
+        routes = self.make_genomic_profiles_routes(dataset_uid, obj_i)
 
-        self.file_def_creators += [cells_file_creator,
-                                   cell_sets_file_creator, genomic_profiles_file_creator]
-        self.routes += self.get_out_dir_route(dataset_uid, obj_i)
+        self.file_def_creators.append(file_def_creator)
+        self.routes += routes
 
-    def create_genomic_multivec_zarr(self, zarr_filepath):
-        in_mtx = self.in_mtx
-        in_clusters_df = self.in_clusters_df
-        in_barcodes_df = self.in_barcodes_df
-        in_bins_df = self.in_bins_df
+    def make_genomic_profiles_routes(self, dataset_uid, obj_i):
+        if self.is_remote:
+            return []
+        else:
+            return self.get_local_dir_route(dataset_uid, obj_i, self._zarr_path, self.local_dir_uid)
 
-        starting_resolution = self.starting_resolution
-
-        # The bin datafram consists of one column like chrName:binStart-binEnd
-        def convert_bin_name_to_chr_name(bin_name):
-            try:
-                return bin_name[:bin_name.index(':')]
-            except ValueError:
-                return np.nan
-
-        def convert_bin_name_to_chr_start(bin_name):
-            try:
-                return int(bin_name[bin_name.index(':') + 1:bin_name.index('-')])
-            except ValueError:
-                return np.nan
-
-        def convert_bin_name_to_chr_end(bin_name):
-            try:
-                return int(bin_name[bin_name.index('-') + 1:])
-            except ValueError:
-                return np.nan
-
-        # The genome assembly is GRCh38 but the chromosome names in the bin names do not start with the "chr" prefix.
-        # This is incompatible with the chromosome names from `negspy`, so we need to append the prefix.
-        in_bins_df[0] = in_bins_df[0].apply(lambda x: "chr" + x)
-
-        in_bins_df["chr_name"] = in_bins_df[0].apply(
-            convert_bin_name_to_chr_name)
-        in_bins_df["chr_start"] = in_bins_df[0].apply(
-            convert_bin_name_to_chr_start)
-        in_bins_df["chr_end"] = in_bins_df[0].apply(
-            convert_bin_name_to_chr_end)
-
-        # Drop any rows that had incorrect bin strings (missing a chromosome name, bin start, or bin end value).
-        in_bins_df = in_bins_df.dropna(
-            subset=["chr_name", "chr_start", "chr_end"]).copy()
-
-        # Ensure that the columns have the expect types.
-        in_bins_df["chr_name"] = in_bins_df["chr_name"].astype(str)
-        in_bins_df["chr_start"] = in_bins_df["chr_start"].astype(int)
-        in_bins_df["chr_end"] = in_bins_df["chr_end"].astype(int)
-
-        # Create the Zarr store for the outputs.
-        out_f = zarr.open(zarr_filepath, mode='w')
-
-        # Get a list of clusters.
-        in_clusters_df["cluster"] = in_clusters_df["cluster"].astype(str)
-        cluster_ids = in_clusters_df["cluster"].unique().tolist()
-        cluster_ids.sort(key=int)
-
-        cluster_paths = [["Clusters", cluster_id]
-                         for cluster_id in cluster_ids]
-
-        # "SnapTools performs quantification using a specified aligner, and HuBMAP has standardized on BWA with the GRCh38 reference genome"
-        # Reference: https://github.com/hubmapconsortium/sc-atac-seq-pipeline/blob/bb023f95ca3330128bfef41cc719ffcb2ee6a190/README.md
-        genomic_profiles = GenomicProfiles(
-            out_f, profile_paths=cluster_paths, assembly='hg38', starting_resolution=starting_resolution)
-        chrom_name_to_length = genomic_profiles.chrom_name_to_length
-
-        # Create each chromosome dataset.
-        for chr_name, chr_len in chrom_name_to_length.items():
-            # The bins dataframe frustratingly does not contain every bin.
-            # We need to figure out which bins are missing.
-
-            # We want to check for missing bins in each chromosome separately,
-            # otherwise too much memory is used during the join step.
-            chr_bins_in_df = in_bins_df.loc[in_bins_df["chr_name"] == chr_name]
-            if chr_bins_in_df.shape[0] == 0:
-                # No processing or output is necessary if there is no data for this chromosome.
-                # Continue on through all resolutions of this chromosome to the next chromosome.
-                continue
-
-            # Determine the indices of the matrix at which the bins for this chromosome start and end.
-            chr_bin_i_start = int(chr_bins_in_df.head(1).iloc[0].name)
-            chr_bin_i_end = int(chr_bins_in_df.tail(1).iloc[0].name) + 1
-
-            # Extract the part of the matrix corresponding to the current chromosome.
-            chr_mtx = in_mtx[:, chr_bin_i_start:chr_bin_i_end]
-
-            # Create a list of the "ground truth" bins (all bins from position 0 to the end of the chromosome).
-            # We will join the input bins onto this dataframe to determine which bins are missing.
-            chr_bins_gt_df = pd.DataFrame()
-            chr_bins_gt_df["chr_start"] = np.arange(0, math.ceil(
-                chr_len / starting_resolution)) * starting_resolution
-            chr_bins_gt_df["chr_end"] = chr_bins_gt_df["chr_start"] + \
-                starting_resolution
-            chr_bins_gt_df["chr_start"] = chr_bins_gt_df["chr_start"] + 1
-            chr_bins_gt_df["chr_start"] = chr_bins_gt_df["chr_start"].astype(
-                int)
-            chr_bins_gt_df["chr_end"] = chr_bins_gt_df["chr_end"].astype(int)
-            chr_bins_gt_df["chr_name"] = chr_name
-            chr_bins_gt_df[0] = chr_bins_gt_df.apply(
-                lambda r: f"{r['chr_name']}:{r['chr_start']}-{r['chr_end']}", axis='columns')
-
-            # We will add a new column "i", which should match the _old_ index, so that we will be able join with the data matrix on the original indices.
-            # For the new rows, we will add values for the "i" column that are greater than any of the original indices,
-            # to prevent any joining with the incoming data matrix onto these bins for which the data is missing.
-            chr_bins_in_df = chr_bins_in_df.reset_index(drop=True)
-            chr_bins_in_df["i"] = chr_bins_in_df.index.values
-            chr_bins_gt_df["i"] = chr_bins_gt_df.index.values + \
-                (in_mtx.shape[1] + 1)
-
-            # Set the full bin string column as the index of both data frames.
-            chr_bins_gt_df = chr_bins_gt_df.set_index(0)
-            chr_bins_in_df = chr_bins_in_df.set_index(0)
-
-            # Join the input bin subset dataframe right onto the full bin ground truth dataframe.
-            chr_bins_in_join_df = chr_bins_in_df.join(
-                chr_bins_gt_df, how='right', lsuffix="", rsuffix="_gt")
-            # The bins which were not present in the input will have NaN values in the "i" column.
-            # For these rows, we replace the NaN values with the much higher "i_gt" values which will not match to any index of the data matrix.
-            chr_bins_in_join_df["i"] = chr_bins_in_join_df.apply(
-                lambda r: r['i'] if pd.notna(r['i']) else r['i_gt'], axis='columns').astype(int)
-
-            # Clean up the joined data frame by removing unnecessary columns.
-            chr_bins_in_join_df = chr_bins_in_join_df.drop(
-                columns=['chr_name', 'chr_start', 'chr_end', 'i_gt'])
-            chr_bins_in_join_df = chr_bins_in_join_df.rename(
-                columns={'chr_name_gt': 'chr_name', 'chr_start_gt': 'chr_start', 'chr_end_gt': 'chr_end'})
-
-            # Create a dataframe from the data matrix, so that we can join to the joined bins dataframe.
-            chr_mtx_df = pd.DataFrame(data=chr_mtx.T)
-
-            chr_bins_i_df = chr_bins_in_join_df.drop(
-                columns=['chr_name', 'chr_start', 'chr_end'])
-
-            # Join the data matrix dataframe and the bins dataframe.
-            # Bins that are missing from the data matrix will have "i" values higher than any of the data matrix dataframe row indices,
-            # and therefore the data values for these bins in the resulting joined dataframe will all be NaN.
-            chr_mtx_join_df = chr_bins_i_df.join(
-                chr_mtx_df, how='left', on='i')
-            # We fill in these NaN values with 0.
-            chr_mtx_join_df = chr_mtx_join_df.fillna(value=0.0)
-
-            # Drop the "i" column, since it is not necessary now that we have done the join.
-            chr_mtx_join_df = chr_mtx_join_df.drop(columns=['i'])
-            # Obtain the new full data matrix, which contains values for all bins of the chromosome.
-            chr_mtx = chr_mtx_join_df.values.T
-
-            # Fill in the Zarr store with data for each cluster.
-            for cluster_index, cluster_id in enumerate(cluster_ids):
-                # Get the list of cells in the current cluster.
-                cluster_df = in_clusters_df.loc[in_clusters_df["cluster"]
-                                                == cluster_id]
-                cluster_cell_ids = cluster_df.index.values.tolist()
-                cluster_cells_tf = (
-                    in_barcodes_df[0].isin(cluster_cell_ids)).values
-
-                # Get the rows of the data matrix corresponding to the cells in this cluster.
-                cluster_cell_by_bin_mtx = chr_mtx[cluster_cells_tf, :]
-                # Take the sum of this cluster along the cells axis.
-                cluster_profile = cluster_cell_by_bin_mtx.sum(axis=0)
-
-                genomic_profiles.add_profile(
-                    cluster_profile, chr_name, cluster_index)
-
-        return
+    def get_zarr_url(self, base_url="", dataset_uid="", obj_i=""):
+        if self.is_remote:
+            return self._zarr_url
+        return self.get_local_dir_url(base_url, dataset_uid, obj_i, self.local_dir_uid)
 
     def make_genomic_profiles_file_def_creator(self, dataset_uid, obj_i):
-        def get_genomic_profiles(base_url):
+        def genomic_profiles_file_def_creator(base_url):
             return {
-                "fileType": ft.GENOMIC_PROFILES_ZARR.value,
-                "url": self._get_url(base_url, dataset_uid, obj_i, self.zarr_folder)
+                "fileType": "genomic-profiles.zarr",
+                "url": self.get_zarr_url(base_url, dataset_uid, obj_i)
             }
-
-        return get_genomic_profiles
-
-    def create_cell_sets_json(self):
-        in_clusters_df = self.in_clusters_df
-        cell_sets = CellSets()
-        cell_sets.add_level_zero_node('Clusters')
-
-        cell_ids = in_clusters_df.index.values.tolist()
-        in_clusters_df['cluster'] = in_clusters_df['cluster'].astype(str)
-        cluster_ids = in_clusters_df['cluster'].unique().tolist()
-        cluster_ids.sort(key=int)
-        cell_cluster_ids = in_clusters_df['cluster'].values.tolist()
-
-        cell_cluster_tuples = list(zip(cell_ids, cell_cluster_ids))
-
-        for cluster_id in cluster_ids:
-            cell_set = [
-                str(cell_id)
-                for cell_id, cell_cluster_id in cell_cluster_tuples
-                if cell_cluster_id == cluster_id
-            ]
-            cell_sets.add_node(str(cluster_id), ['Clusters'], cell_set)
-
-        return cell_sets.json
-
-    def make_cell_sets_file_def_creator(self, dataset_uid, obj_i):
-        def get_cell_sets(base_url):
-            return {
-                "fileType": ft.CELL_SETS_JSON.value,
-                "url": self._get_url(base_url, dataset_uid, obj_i, "cell-sets")
-            }
-        return get_cell_sets
-
-    def create_cells_json(self):
-        in_clusters_df = self.in_clusters_df
-
-        cell_ids = in_clusters_df.index.tolist()
-        cells = Cells(cell_ids=cell_ids)
-        mapping = in_clusters_df[["umap.1", "umap.2"]].values.tolist()
-        cells.add_mapping("UMAP", mapping)
-        return cells.json
-
-    def make_cells_file_def_creator(self, dataset_uid, obj_i):
-        def get_cells(base_url):
-
-            return {
-                "fileType": ft.CELLS_JSON.value,
-                "url": self._get_url(base_url, dataset_uid, obj_i, "cells")
-            }
-        return get_cells
-
-    def auto_view_config(self, vc):
-        dataset = vc.add_dataset().add_object(self)
-        genomic_profiles = vc.add_view(cm.GENOMIC_PROFILES, dataset=dataset)
-        scatter = vc.add_view(cm.SCATTERPLOT, dataset=dataset, mapping="UMAP")
-        cell_sets = vc.add_view(cm.OBS_SETS, dataset=dataset)
-
-        vc.layout(genomic_profiles / (scatter | cell_sets))
+        return genomic_profiles_file_def_creator

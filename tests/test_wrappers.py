b2b4ffe9e0cf16457599ca86a528694c6628fce1
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-02-15 11:35:07 -0500

    Update tests

--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -1,9 +1,7 @@
 import unittest
 
-import zarr
 from anndata import read_h5ad
-from scipy.io import mmread
-import pandas as pd
+from pathlib import Path
 
 from .create_test_data import (
     create_test_anndata_file,
@@ -17,11 +15,9 @@ from vitessce import (
     OmeTiffWrapper,
     OmeZarrWrapper,
     AnnDataWrapper,
-    SnapWrapper,
     CsvWrapper,
+    MultivecZarrWrapper,
 )
-
-from pathlib import Path
 
 
 data_path = Path('tests/data')
@@ -128,42 +124,18 @@ class TestWrappers(unittest.TestCase):
             }
         })
 
-    def test_snaptools(self):
-        mtx = mmread(data_path / 'test.snap.mtx')
-        barcodes_df = pd.read_csv(
-            data_path / 'test.snap.barcodes.txt', header=None)
-        bins_df = pd.read_csv(
-            data_path / 'test.snap.bins.txt', header=None)
-        clusters_df = pd.read_csv(
-            data_path / 'test.snap.clusters.csv', index_col=0)
-
+    def test_multivec_zarr(self):
         zarr_filepath = data_path / 'test_out.snap.multivec.zarr'
 
-        w = SnapWrapper(mtx, barcodes_df, bins_df, clusters_df)
-        w.create_genomic_multivec_zarr(zarr_filepath)
+        w = MultivecZarrWrapper(zarr_path=zarr_filepath)
+        w.local_dir_uid = 'test_uid'
 
-        z = zarr.open(zarr_filepath, mode='r')
-
-        self.assertEqual(z['chromosomes/chr1/5000'].shape, (4, 49792))
-        self.assertEqual(z['chromosomes/chr1/5000'][:, 0].sum(), 0)
-        self.assertEqual(z['chromosomes/chr1/5000'][:, 1].sum(), 0)
-        self.assertEqual(z['chromosomes/chr1/5000'][:, 2].sum(), 7)
-        self.assertEqual(z['chromosomes/chr1/5000'][:, 3].sum(), 7)
-        self.assertEqual(z['chromosomes/chr1/5000'][:, 4].sum(), 0)
-        self.assertEqual(z['chromosomes/chr1/5000'][0, :].sum(), 17)
-        self.assertEqual(z['chromosomes/chr1/10000'][0, :].sum(), 17)
-        self.assertEqual(z['chromosomes/chr1/5000'][:, 2].sum(), 7)
-        self.assertEqual(z['chromosomes/chr2/5000'][:, 0].sum(), 0)
-        self.assertEqual(z['chromosomes/chr2/5000'][:, 1].sum(), 0)
-        self.assertEqual(z['chromosomes/chr2/10000'][:, 0].sum(), 0)
-        self.assertEqual(z['chromosomes/chr2/5000'][:, 2].sum(), 4)
-        self.assertEqual(z['chromosomes/chr2/5000'][:, 3].sum(), 9)
-        self.assertEqual(z['chromosomes/chr2/10000'][:, 1].sum(), 13)
-        self.assertEqual(z['chromosomes/chr3/5000'][:, 3].sum(), 9)
-        self.assertEqual(z['chromosomes/chr3/5000'][:].sum(), 9)
-        self.assertEqual(z['chromosomes/chr18/5000'][:].sum(), 8)
-
-        cells_json = w.create_cells_json()
-        self.assertEqual(len(cells_json), 6)
-        self.assertEqual(cells_json['AAACATCGAGTACAAGACAGCAGA'], {
-                         'mappings': {'UMAP': [4.43, 1.64]}})
+        file_def_creator = w.make_genomic_profiles_file_def_creator(
+            "A",
+            "0"
+        )
+        file_def = file_def_creator('http://localhost:8000')
+        self.assertEqual(file_def, {
+            'fileType': 'genomic-profiles.zarr',
+            'url': 'http://localhost:8000/A/0/test_uid',
+        })

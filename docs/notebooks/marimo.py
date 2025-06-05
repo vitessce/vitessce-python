84bf88a248b30d47351e4bffc2e5184c1f4d3c82
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-06-05 09:58:29 -0400

    Add marimo nb

---
+++ docs/notebooks/marimo.py
@@ -0,0 +1,52 @@
+import marimo
+
+__generated_with = "0.13.15"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _():
+    from vitessce import VitessceConfig
+    from example_configs import dries as dries_config
+    return VitessceConfig, dries_config
+
+
+@app.cell
+def _(VitessceConfig, dries_config, mo):
+    vc = VitessceConfig.from_dict(dries_config)
+    vw = mo.ui.anywidget(vc.widget())
+    vw
+    return (vw,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""As you interact with the widget above, watch as the Vitessce configuration below dynamically updates. For instance, as you zoom in the scatterplot, check the value of `coordinationSpace.embeddingZoom`""")
+    return
+
+
+@app.cell
+def _(vw):
+    vw.config
+    return
+
+
+@app.cell
+def _(vw):
+    vw.config["coordinationSpace"]["embeddingZoom"]
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()

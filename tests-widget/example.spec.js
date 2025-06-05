227afd47c537ed04034a02e2c408208ce5aa620a
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-06-05 11:52:12 -0400

    Add test for marimo html export (#439)
    * Add test for marimo html export
    
    * Extras

--- tests-widget/example.spec.js
+++ tests-widget/example.spec.js
@@ -1,12 +1,23 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('Renders Vitessce widget containing a scatterplot view', async ({ page }) => {
+test('Renders Vitessce widget containing a scatterplot view (Jupyter)', async ({ page }) => {
   test.setTimeout(60_000);
   await page.goto('http://localhost:3000/widget_from_dict.html');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('widget_from_dict');
+
+  await expect(page.getByText('Scatterplot (UMAP)')).toBeVisible();
+  await expect(page.getByText('523 cells')).toHaveCount(3);
+});
+
+test('Renders Vitessce widget containing a scatterplot view (Marimo)', async ({ page }) => {
+  test.setTimeout(60_000);
+  await page.goto('http://localhost:3000/marimo.html');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('marimo');
 
   await expect(page.getByText('Scatterplot (UMAP)')).toBeVisible();
   await expect(page.getByText('523 cells')).toHaveCount(3);

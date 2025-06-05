6bb8c7b6365cb60b8a0d372e36013d1031663e56
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-06-05 10:01:15 -0400

    Notebook which demonstrates neuroglancer view (#437)
    * WIP
    
    * Add marimo nb

--- src/vitessce/widget.py
+++ src/vitessce/widget.py
@@ -497,7 +497,8 @@ async function render(view) {
     const root = createRoot(view.el);
     // Marimo puts AnyWidgets in a Shadow Root, so we need to tell Emotion to
     // insert styles within the Shadow DOM.
-    const styleContainer = view.el.getRootNode();
+    const rootNode = view.el.getRootNode();
+    const styleContainer = rootNode === document ? undefined : rootNode;
     root.render(e(VitessceWidget, { model: view.model, styleContainer }));
 
     return () => {
@@ -601,7 +602,7 @@ class VitessceWidget(anywidget.AnyWidget):
 
     next_port = DEFAULT_PORT
 
-    js_package_version = Unicode('3.5.12').tag(sync=True)
+    js_package_version = Unicode('3.6.2').tag(sync=True)
     js_dev_mode = Bool(False).tag(sync=True)
     custom_js_url = Unicode('').tag(sync=True)
     plugin_esm = List(trait=Unicode(''), default_value=[]).tag(sync=True)
@@ -613,7 +614,7 @@ class VitessceWidget(anywidget.AnyWidget):
 
     store_urls = List(trait=Unicode(''), default_value=[]).tag(sync=True)
 
-    def __init__(self, config, height=600, theme='auto', uid=None, port=None, proxy=False, js_package_version='3.5.12', js_dev_mode=False, custom_js_url='', plugins=None, remount_on_uid_change=True, prefer_local=True, invoke_timeout=300000, invoke_batched=True, page_mode=False, page_esm=None):
+    def __init__(self, config, height=600, theme='auto', uid=None, port=None, proxy=False, js_package_version='3.6.2', js_dev_mode=False, custom_js_url='', plugins=None, remount_on_uid_change=True, prefer_local=True, invoke_timeout=300000, invoke_batched=True, page_mode=False, page_esm=None):
         """
         Construct a new Vitessce widget.
 
@@ -748,7 +749,7 @@ class VitessceWidget(anywidget.AnyWidget):
 # Launch Vitessce using plain HTML representation (no ipywidgets)
 
 
-def ipython_display(config, height=600, theme='auto', base_url=None, host_name=None, uid=None, port=None, proxy=False, js_package_version='3.5.12', js_dev_mode=False, custom_js_url='', plugins=None, remount_on_uid_change=True, page_mode=False, page_esm=None):
+def ipython_display(config, height=600, theme='auto', base_url=None, host_name=None, uid=None, port=None, proxy=False, js_package_version='3.6.2', js_dev_mode=False, custom_js_url='', plugins=None, remount_on_uid_change=True, page_mode=False, page_esm=None):
     from IPython.display import display, HTML
     uid_str = "vitessce" + get_uid_str(uid)
 

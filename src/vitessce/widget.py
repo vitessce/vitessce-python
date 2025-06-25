3226310345da6c7f89ed715c1cc5843f52440dc8
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-06-25 14:37:25 -0400

    Update scroll behavior in pageMode

--- src/vitessce/widget.py
+++ src/vitessce/widget.py
@@ -424,7 +424,7 @@ async function render(view) {
         const divRef = React.useRef();
 
         React.useEffect(() => {
-            if(!divRef.current) {
+            if(!divRef.current || pageMode) {
                 return () => {};
             }
 
@@ -450,7 +450,7 @@ async function render(view) {
                     divRef.current.removeEventListener("mouseleave", handleMouseLeave);
                 }
             };
-        }, [divRef]);
+        }, [divRef, pageMode]);
 
         // Config changed on JS side (from within <Vitessce/>),
         // send updated config to Python side.

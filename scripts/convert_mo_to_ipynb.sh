084cfacbe3029438190fb3ddccaacd79324c0fe9
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-08-08 11:48:44 -0400

    Script options

--- scripts/convert_mo_to_ipynb.sh
+++ scripts/convert_mo_to_ipynb.sh
@@ -2,7 +2,7 @@ cd docs/notebooks
 
 for file in *.mo.py; do
     echo "Converting $file to ${file%.mo.py}.ipynb"
-    marimo export ipynb "$file" --output "__ipynb__/${file%.mo.py}.ipynb" --force
+    uv run marimo export ipynb —-sort=top-down --force --output "__ipynb__/${file%.mo.py}.ipynb" "$file"
 done
 
 cp example_configs.py "__ipynb__/example_configs.py"

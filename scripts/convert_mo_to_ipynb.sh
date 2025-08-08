6b6061d7113cdf6520409bca24484dad8c495973
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2025-08-08 11:28:18 -0400

    Update

--- scripts/convert_mo_to_ipynb.sh
+++ scripts/convert_mo_to_ipynb.sh
@@ -4,3 +4,5 @@ for file in *.mo.py; do
     echo "Converting $file to ${file%.mo.py}.ipynb"
     marimo export ipynb "$file" --output "__ipynb__/${file%.mo.py}.ipynb" --force
 done
+
+cp example_configs.py "__ipynb__/example_configs.py"

455944bb18b8ca89505aed382ed0aaee5f15b2bb
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-02-05 12:40:45 -0500

    Update script

--- tests/create_xenium_filtered_points.py
+++ tests/create_xenium_filtered_points.py
@@ -8,44 +8,48 @@ import shutil
 from spatialdata import read_zarr, SpatialData
 
 
-data_dir = "data"
-zip_filepath = join(data_dir, "xenium_rep1_io.spatialdata.zarr.zip")
-spatialdata_filepath = join(data_dir, "xenium_rep1_io.spatialdata.zarr")
+def create_xenium_filtered_points():
+    # 1. Download and extract the Xenium dataset if not already present
+    data_dir = "data"
+    zip_filepath = join(data_dir, "xenium_rep1_io.spatialdata.zarr.zip")
+    spatialdata_filepath = join(data_dir, "xenium_rep1_io.spatialdata.zarr")
 
 
-if not isdir(spatialdata_filepath):
-    if not isfile(zip_filepath):
-        os.makedirs(data_dir, exist_ok=True)
-        urlretrieve('https://s3.embl.de/spatialdata/spatialdata-sandbox/xenium_rep1_io.zip', zip_filepath)
-    with zipfile.ZipFile(zip_filepath, "r") as zip_ref:
-        zip_ref.extractall(data_dir)
-        os.rename(join(data_dir, "data.zarr"), spatialdata_filepath)
+    if not isdir(spatialdata_filepath):
+        if not isfile(zip_filepath):
+            os.makedirs(data_dir, exist_ok=True)
+            # zip_url = 'https://s3.embl.de/spatialdata/spatialdata-sandbox/xenium_rep1_io.zip'
+            zip_url = 'https://s3.embl.de/spatialdata/spatialdata-sandbox/xenium_rep1_io_spatialdata_0.7.1.zip'
+            urlretrieve(zip_url, zip_filepath)
+        with zipfile.ZipFile(zip_filepath, "r") as zip_ref:
+            zip_ref.extractall(data_dir)
+            os.rename(join(data_dir, "data.zarr"), spatialdata_filepath)
 
-        # This Xenium dataset has an AnnData "raw" element.
-        # Reference: https://github.com/giovp/spatialdata-sandbox/issues/55
-        raw_dir = join(spatialdata_filepath, "tables", "table", "raw")
-        if isdir(raw_dir):
-            shutil.rmtree(raw_dir)
+            # This Xenium dataset has an AnnData "raw" element.
+            # Reference: https://github.com/giovp/spatialdata-sandbox/issues/55
+            raw_dir = join(spatialdata_filepath, "tables", "table", "raw")
+            if isdir(raw_dir):
+                shutil.rmtree(raw_dir)
 
-sdata = read_zarr(spatialdata_filepath)
+    sdata = read_zarr(spatialdata_filepath)
 
-ddf = sdata.points["transcripts"]
+    ddf = sdata.points["transcripts"]
 
-# 2. Define a function to take every 100th row from a partition
-
-
-def select_every_200th(partition):
-    # Each 'partition' is a Pandas DataFrame
-    # .iloc[::100] is the efficient pandas way to get every 100th row
-    return partition.iloc[::200]
+    # 2. Define a function to take every 100th row from a partition
 
 
-# 3. Apply this function to every partition in the Dask DataFrame
-result = ddf.map_partitions(select_every_200th)
+    def select_every_200th(partition):
+        # Each 'partition' is a Pandas DataFrame
+        # .iloc[::100] is the efficient pandas way to get every 100th row
+        return partition.iloc[::200]
 
-# 4. Compute the result to see it
-filtered_ddf = result[["x", "y", "z", "feature_name", "cell_id"]]
 
-small_sdata = SpatialData(points={"transcripts": filtered_ddf})
+    # 3. Apply this function to every partition in the Dask DataFrame
+    result = ddf.map_partitions(select_every_200th)
 
-small_sdata.write("xenium_rep1_io.points_only.spatialdata.zarr", overwrite=True)
+    # 4. Compute the result to see it
+    filtered_ddf = result[["x", "y", "z", "feature_name", "cell_id"]]
+
+    small_sdata = SpatialData(points={"transcripts": filtered_ddf})
+
+    small_sdata.write("xenium_rep1_io.points_only.spatialdata.zarr", overwrite=True)
